81f302ed1216384b9495b4acc37e460845c07094
tsukaryu <drgon8otsuka@gmail.com>
2021-04-22 16:07:52 +0900

    add GetInTouch

--- src/components/TopPage/GetInTouch/GetInTouch.js
+++ src/components/TopPage/GetInTouch/GetInTouch.js
@@ -1,6 +1,45 @@
 import React from 'react';
-// import classes from './GetInTouch.module.css';
+import classes from './GetInTouch.module.css';
+
+import window from '../../../images/getintouchsvg/window.svg';
+import twitter from '../../../images/twitter.svg';
+import github from '../../../images/github.svg';
 
 export default function GetInTouch() {
-  return <div>GetInTouch</div>;
+  return (
+    <div className={classes.GetInTouch}>
+      <div className={classes.GetInTouch__Inner}>
+        <div className={classes.GetInTouch__texts}>
+          <div className={classes.GetInTouch__text__title}>
+            <h1>Get In Touch</h1>
+          </div>
+          <div className={classes.GetInTouch__text__link}>
+            <div className={classes.GetInTouch__text__linkitem}>
+              <a href='#/' alt=''>
+                <p>drgon8otsuka@gmail.com</p>
+              </a>
+            </div>
+            <div className={classes.GetInTouch__text__linkitem}>
+              <a href='#/' alt=''>
+                <p>Wantedly</p>
+              </a>
+            </div>
+            <div className={classes.GetInTouch__text__linkitem}>
+              <a href='#/'>
+                <img className={classes.twitter} src={twitter} alt='' />
+              </a>
+              <a href='#/'>
+                <img className={classes.github} src={github} alt='' />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className={classes.GetInTouch__img}>
+          <div className={classes.window}>
+            <img src={window} alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/TopPage/Works/Works.js
+++ src/components/TopPage/Works/Works.js
@@ -1,6 +1,39 @@
 import React from 'react';
-// import classes from './Works.module.css';
+import classes from './Works.module.css';
+
+import astronaut from '../../../images/workssvg/astronaut.svg';
+import rocket from '../../../images/workssvg/rocket.svg';
+import planet from '../../../images/workssvg/planet.svg';
 
 export default function Works() {
-  return <div>Works</div>;
+  return (
+    <div className={classes.Works}>
+      <div className={classes.Works__Inner}>
+        <div className={classes.Works__texts}>
+          <div className={classes.Works__text__title}>
+            <h1>Works</h1>
+          </div>
+          <div className={classes.Works__text__lead}>
+            <p>これまで制作した成果物です。</p>
+          </div>
+          <div className={classes.Works__text__btn}>
+            <a href='#/' alt=''>
+              <span>read more</span>
+            </a>
+          </div>
+        </div>
+        <div className={classes.Works__img}>
+          <div className={classes.planet}>
+            <img src={planet} alt='' />
+          </div>
+          <div className={classes.rocket}>
+            <img src={rocket} alt='' />
+          </div>
+          <div className={classes.astronaut}>
+            <img src={astronaut} alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
