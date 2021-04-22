746928ebe5ac0e7a35b4387873dd64e31e3601a0
tsukaryu <drgon8otsuka@gmail.com>
2021-04-22 11:57:14 +0900

    add Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,44 @@
 import React from 'react';
-// import classes from './Header.module.css';
+import classes from './Header.module.css';
+
+import { HamburgerSpin } from 'react-animated-burgers';
+import twitter from '../../images/twitter.svg';
+import github from '../../images/github.svg';
+import '@fontsource/orbitron';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={classes.Header}>
+      <div className={classes.Header__Inner}>
+        <div>
+          <a href='#/'>tsuka-ryu</a>
+        </div>
+        <ul>
+          <li>
+            <a href='#/'>
+              <img className={classes.twitter} src={twitter} alt='' />
+            </a>
+          </li>
+          <li>
+            <a href='#/'>
+              <img className={classes.github} src={github} alt='' />
+            </a>
+          </li>
+          <li>
+            <a href='#/'>Wantedly</a>
+          </li>
+          <li>
+            <a href='#/'>
+              <HamburgerSpin
+                className={classes.hamburgerspin}
+                barColor='#fff'
+                isActive={false}
+                toggleButton={() => {}}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
