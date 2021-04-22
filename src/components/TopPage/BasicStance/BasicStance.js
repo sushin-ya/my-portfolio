f8b39fd999e8538c1fe1ee1187e7dc9322eb2761
tsukaryu <drgon8otsuka@gmail.com>
2021-04-22 15:49:31 +0900

    add Basic Stance

--- src/components/TopPage/BasicStance/BasicStance.js
+++ src/components/TopPage/BasicStance/BasicStance.js
@@ -1,6 +1,32 @@
 import React from 'react';
-// import classes from './BasicStance.module.css';
+import classes from './BasicStance.module.css';
+
+import moonrocket from '../../../images/basicstancesvg/moonrocket.svg';
 
 export default function BasicStance() {
-  return <div>BasicStance</div>;
+  return (
+    <div className={classes.BasicStance}>
+      <div className={classes.BasicStance__Inner}>
+        <div className={classes.BasicStance__texts}>
+          <div className={classes.BasicStance__text__title}>
+            <h1>Basic Stance</h1>
+          </div>
+          <div className={classes.BasicStance__text__lead}>
+            <p>基本的な考え方についてです。</p>
+            <p>technology / design / culture</p>
+          </div>
+          <div className={classes.BasicStance__text__btn}>
+            <a href='#/' alt=''>
+              <span>read more</span>
+            </a>
+          </div>
+        </div>
+        <div className={classes.BasicStance__img}>
+          <div className={classes.moonrocket}>
+            <img src={moonrocket} alt='' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
