1153e192c7b31865c370ea96f23a7c0c1ee11d70
tsukaryu <drgon8otsuka@gmail.com>
2021-04-24 09:03:47 +0900

    add all pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import TopPage from './components/TopPage/TopPage';
+import AboutMePage from './components/AboutMePage/AboutMePage';
+import BasicStancePage from './components/BasicStancePage/BasicStancePage';
+import WorksPage from './components/WorksPage/WorksPage';
 
 export default function App() {
   return (
     <div className='App'>
-      <Header />
-      <TopPage />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/aboutme' component={AboutMePage} />
+          <Route path='/basicstance' component={BasicStancePage} />
+          <Route path='/works' component={WorksPage} />
+          <Route path='/' component={TopPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/BasicStancePage/BasicStancePage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function BasicStancePage() {
+  return <div>BasicStancePage</div>;
+}
